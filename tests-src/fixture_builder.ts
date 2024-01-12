fce725d7483c45a54a7ef3116119a88f7c8cd2ca
Josh Pendragon <pendragon.josh@gmail.com>
2024-01-12 13:13:10 -0600

    convert page logic to POM + fixture

--- tests-src/fixture_builder.ts
+++ tests-src/fixture_builder.ts
@@ -1,11 +1,27 @@
-import {test as base, expect} from '@playwright/test';
+import {test as base, expect, mergeTests} from '@playwright/test';
+import {basePageFactory, BasePage} from "./POMs/pages";
 
-export const test = base.extend({
+const overrideTest = base.extend({
   page: async ({baseURL, page}, use) => {
     const headshot = page.getByTestId('Headshot');
     await page.goto(baseURL);
-    await expect(page).toHaveTitle(/Pendragon Portfolio/);
-    await expect(headshot).toBeVisible();
+    await expect(page)
+      .toHaveTitle(/Pendragon Portfolio/);
+    await expect(headshot)
+      .toBeVisible();
     await use(page);
-  }
+  },
 })
+
+const fixtureTest = base.extend <
+  {
+    basePage: BasePage
+  }
+>({
+  basePage: [async ({page}, use) => {
+    const bp = basePageFactory(page);
+    await use(bp);
+  }, {scope: 'test'}]
+})
+
+export const test = mergeTests(overrideTest, fixtureTest);
--- tests/smoke_and_nav.spec.ts
+++ tests/smoke_and_nav.spec.ts
@@ -1,10 +1,9 @@
 import {expect} from '@playwright/test';
 import {test} from "../tests-src/fixture_builder";
-import {goToPage, pages} from "../tests-src/pages";
 
-test('check routes', async ({page}) => {
-    for (const route of pages) {
-        await goToPage(page, route.name);
+test('check routes', async ({page, basePage}) => {
+    for (const route of basePage.pages) {
+        await basePage.goTo(route.name)
         await expect(async () => {
             expect(page.url(), `Page ${route.name} did not load correctly`)
                 .toMatch(route.url)
@@ -14,12 +13,11 @@ test('check routes', async ({page}) => {
     }
 });
 
-test('check links', async ({page}) => {
-    for (const route of pages) {
-        const navLink = page
-            .getByTestId('NavTree')
-            .locator('a', {hasText: route.name})
-        await expect(navLink, `Link with name ${route.name} should be visible.`).toBeVisible()
+test('check links', async ({page, basePage}) => {
+    for (const route of basePage.pages) {
+        const navLink = basePage.getNavLink(route.name)
+        await expect(navLink, `Link with name ${route.name} should be visible.`)
+            .toBeVisible()
         await navLink.click()
         await expect(page.url(),
             `Url for ${route.name} should match ${route.url}.`
