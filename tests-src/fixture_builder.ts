f5b755e662ec6c33f83a2418ab955311cd79263f
Josh Pendragon <pendragon.josh@gmail.com>
2024-01-12 12:53:00 -0600

    add sanity checks for initial page load

--- tests-src/fixture_builder.ts
+++ tests-src/fixture_builder.ts
@@ -2,8 +2,10 @@ import {test as base, expect} from '@playwright/test';
 
 export const test = base.extend({
   page: async ({baseURL, page}, use) => {
+    const headshot = page.getByTestId('Headshot');
     await page.goto(baseURL);
     await expect(page).toHaveTitle(/Pendragon Portfolio/);
+    await expect(headshot).toBeVisible();
     await use(page);
   }
 })
